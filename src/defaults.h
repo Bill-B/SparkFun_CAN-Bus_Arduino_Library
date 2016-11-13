b68175761fa0426fe33e7214195207f234284ca3
Bill-B <toronto_4@hotmail.com>
2016-11-12 19:07:07 -0500

    Added comments to pinout

--- src/defaults.h
+++ src/defaults.h
@@ -1,14 +1,14 @@
 #ifndef	DEFAULTS_H
 #define	DEFAULTS_H
 
-#define	P_MOSI	B,3
-#define	P_MISO	B,4
-#define	P_SCK	B,5
+#define	P_MOSI	B,3 //pin 11
+#define	P_MISO	B,4 //pin 12
+#define	P_SCK	B,5 //pin 13
 
 //#define	MCP2515_CS			D,3	// Rev A
-#define	MCP2515_CS			B,2 // Rev B
-#define	MCP2515_INT			D,2
-#define LED2_HIGH			B,0
-#define LED2_LOW			B,0
+#define	MCP2515_CS			B,2 // Rev B //pin 10
+#define	MCP2515_INT			D,2 //pin 2
+#define LED2_HIGH			B,0 //pin 8 
+#define LED2_LOW			B,0 //pin 8
 
 #endif	// DEFAULTS_H
